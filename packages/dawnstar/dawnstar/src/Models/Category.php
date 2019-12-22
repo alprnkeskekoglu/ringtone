efe09bbee9f57d13a0b7c1a65466b7cdc4ea3872
valtheim <keskekoglualperen@gmail.com>
2019-12-22 23:32:58 +0300

    revize

--- packages/dawnstar/dawnstar/src/Models/Category.php
+++ packages/dawnstar/dawnstar/src/Models/Category.php
@@ -26,10 +26,10 @@ class Category extends Model
         return $this->hasMany(Tag::class);
     }
     public function children() {
-        return $this->hasMany(Category::class, 'parent_id');
+        return $this->hasMany(Category::class, 'id', 'parent_id');
     }
     public function parent() {
-        return $this->belongsTo(Category::class, 'id', 'parent_id');
+        return $this->belongsTo(Category::class, 'parent_id', 'id');
     }
 
 }
